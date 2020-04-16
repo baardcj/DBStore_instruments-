49e237dc413e218bc93b9a2b8748cbebb3d29ad5
baardcj <baardcj@outlook.com>
2020-04-16 21:19:37 +0200

    Specifies type of producer

---
+++ JPAInstrumentDB/src/main/java/myapp/data/enums/product/production/ProducerType.java
@@ -0,0 +1,10 @@
+package myapp.data.enums.product.production;
+
+public enum ProducerType {
+	big_scale_manufacturer, 
+	medium_scale_manufacturer, 
+	small_scale_manufacturer, 
+	luthier, 
+	layman	
+}
+
