071bf858e0697fe63998e862e2cd32dbace4b87f
baardcj <baardcj@outlook.com>
2020-04-20 13:29:15 +0200

    added table unique constraint. Not tested!

--- JPAInstrumentDB/src/main/java/myapp/data/tables/guitarModels/ElGuitarModel.java
+++ JPAInstrumentDB/src/main/java/myapp/data/tables/guitarModels/ElGuitarModel.java
@@ -2,6 +2,8 @@ package myapp.data.tables.guitarModels;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -10,6 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access=AccessLevel.PUBLIC, force=true) 
 @Entity
+
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {
+		"brand", 
+		"guitarSeries", 
+		"bodyShape"}))
 public class ElGuitarModel {
 	
 	@Id
