fb272de8ae59f6d851ce7a799d5f99adc724477a
baardcj <baardcj@outlook.com>
2020-04-20 13:25:32 +0200

    removed comments

--- JPAInstrumentDB/src/main/java/myapp/data/enums/product/guitar/GuitarSeries.java
+++ JPAInstrumentDB/src/main/java/myapp/data/enums/product/guitar/GuitarSeries.java
@@ -1,5 +1,13 @@
 package myapp.data.enums.product.guitar;
 
+/*
+ * This enum represent permitted values for field in GuitarModel.class. 
+ * 
+ * This enum is for testing and should contain for now all series.
+ * Future work: the guitar series field should only be assigned values consistent with 
+ * guitar brand in GuitarModel.
+ */
+
 public enum GuitarSeries {
 	STANDARD,
 	CUSTOM_SHOP,
