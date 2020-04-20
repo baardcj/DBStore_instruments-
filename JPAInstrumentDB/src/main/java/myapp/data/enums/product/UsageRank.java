bf9f05ce179510e1ce78d67780dd16ce40874b03
baardcj <baardcj@outlook.com>
2020-04-20 13:23:46 +0200

    new name

---
+++ JPAInstrumentDB/src/main/java/myapp/data/enums/product/UsageRank.java
@@ -0,0 +1,12 @@
+package myapp.data.enums.product;
+
+public enum UsageRank {
+	NEW, 				// not owned privately
+	DEMO_OBJECT,  		// not owned privately , but somewhat used
+	USED_PRISTINE, 		// hardly touched
+	USED_MINT, 			// almost as new 
+	MINOR_USE, 			
+	AVERAGE_USE,		
+	HEAVILY_USE
+}
+
