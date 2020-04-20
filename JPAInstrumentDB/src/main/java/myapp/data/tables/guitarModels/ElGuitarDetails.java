0a2314c1a4efd3e9a66d22fa2f63847384c6352d
baardcj <baardcj@outlook.com>
2020-04-20 13:30:10 +0200

    New fields. To evaluate: Some fields may be moved to GuitarModel.class

--- JPAInstrumentDB/src/main/java/myapp/data/tables/guitarModels/ElGuitarDetails.java
+++ JPAInstrumentDB/src/main/java/myapp/data/tables/guitarModels/ElGuitarDetails.java
@@ -1,20 +1,43 @@
 package myapp.data.tables.guitarModels;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import myapp.data.tables.GuitarDetails;
 
 
 @AllArgsConstructor
 @NoArgsConstructor(access=AccessLevel.PUBLIC, force=true) 
+@Data
 @Entity
-public class ElGuitarDetails {
+public class ElGuitarDetails extends GuitarDetails {
+
+
+	/*
+	 * SOME OF THESE FIELDS SHOULD BE PUT IN GUITAR MODEL
+	 * 
+	 */
+	private String bodyWood; 
 	
-	@Id
-	private String name; 
+	private String bodyTopWood; 
+	
+	private String neckWood; 
+	
+	private String fretBoardWood; 
+	
+	private String neckStyle; 
+	
+	private String bridgeType; 
+	
+	
+	private String tunerType; 
+	
+	private String picupConfiguration; 
+	
+	private String electrnoicSetup; 
+	
 	
 }
