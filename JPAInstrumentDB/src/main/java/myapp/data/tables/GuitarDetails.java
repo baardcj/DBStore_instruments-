9224fe1e6232e279f95a002e727922c0189e5b89
baardcj <baardcj@outlook.com>
2020-04-20 13:27:40 +0200

    new fields

--- JPAInstrumentDB/src/main/java/myapp/data/tables/GuitarDetails.java
+++ JPAInstrumentDB/src/main/java/myapp/data/tables/GuitarDetails.java
@@ -1,5 +1,32 @@
 package myapp.data.tables;
 
-public class GuitarDetails {
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PUBLIC, force=true) 
+@Inheritance(strategy=InheritanceType.JOINED)
+@DiscriminatorColumn(name="INSTRUMENT_TYPE_DETAILS")
+@Table(name="GUITAR_DETAILS")
+public abstract class GuitarDetails {
+	
+	@Id
+	private Long id; 
+	
+	private boolean leftHand; 
+	
+	private String nrOfString; 
+	
+	private String scale; 
+	
+	private boolean modified; 
+	
 }
